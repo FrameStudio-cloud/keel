92adc5c1f4a018b8e49396f933baecaa115832c1
lewis irungu <lewisirung489@gmail.com>
2026-07-13 20:46:06 +0300

    Add Vercel Analytics integration to track user interactions

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import './lib/posthog'
 import App from './App.jsx'
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </StrictMode>,
 )
