8956ddf13f6cf13e73daeb2cc0b27ae2f460ac41
lewis irungu <lewisirung489@gmail.com>
2026-07-15 23:21:33 +0300

    fix: add node globals for vite.config.js in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,12 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    files: ['vite.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
@@ -14,7 +20,7 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
+      globals: { ...globals.browser },
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
     rules: {
