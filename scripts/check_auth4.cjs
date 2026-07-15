ce16ad2a522b5e76973d25111f420e05d379e646
lewis irungu <lewisirung489@gmail.com>
2026-07-15 13:43:23 +0300

    cleanup: remove old one-off scripts and legacy keel icon.png
    - Delete 17 unused scripts/ files (checks, migrations, generators)
    - Remove old public/keel icon.png (replaced by keel-icon.png)

--- scripts/check_auth4.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('dist/assets/index-wI2ugOFt.js', 'utf-8');
-
-// Find the full _initSupabaseAuthClient method
-const idx = 467773;
-const methodBody = c.slice(idx, idx + 1000);
-console.log(methodBody);
--- scripts/check_en.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('dist/assets/index-wI2ugOFt.js', 'utf-8');
-
-// Find EN function
-const enDef = c.indexOf('EN=');
-if (enDef >= 0) {
-  console.log('EN= at', enDef, ':', c.slice(enDef, enDef + 200));
-}
